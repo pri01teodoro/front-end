1f2484c28e4376c35b1c41ac5f59c96af9e566fd
priscila.teodoro <priteodoro01@gmail.com>
2022-10-06 22:32:22 -0400

    Iniciando responsividade

---
+++ cursos/responsivo/assets/scripts/menu.js
@@ -0,0 +1,5 @@
+let menu = document.querySelector("#menu")
+
+function showMenu(){
+    menu.classList.toggle('showMenu') /* lista de classes que o menu tem (classList); o add adiciona a classe showMenu dentro de menu */
+}
