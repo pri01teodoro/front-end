d64e864979e46f4e6716c9edfc9e370509278676
priscila.teodoro <priteodoro01@gmail.com>
2022-10-11 22:19:03 -0400

    Melhorando o código

--- cursos/responsivo/assets/scripts/menu.js
+++ cursos/responsivo/assets/scripts/menu.js
@@ -1,5 +1,19 @@
-let menu = document.querySelector("#menu")
+const menu = document.querySelector('#menu');
+const menuIcon = document.querySelector('#menu-icon');
 
 function showMenu(){
-    menu.classList.toggle('showMenu') /* lista de classes que o menu tem (classList); o add adiciona a classe showMenu dentro de menu */
+    menu.classList.toggle('show') /* lista de classes que o menu tem (classList); o add adiciona a classe showMenu dentro de menu */
+    changeMenuIcon();
+}
+
+menuIcon.addEventListener('click', showMenu);
+
+function changeMenuIcon() {
+    if (menu.classList.contains('show')) {
+        menuIcon.src = 'assets/imgs/iconCloseMenu.svg';
+        menuIcon.title = 'Fechar menu';
+    } else {
+        menuIcon.src = 'assets/imgs/iconMenu.svg';
+        menuIcon.title = 'Mostrar menu';
+    }
 }
