31575f2d685e0143ec3d5859de216a095f7e5bb2
priscila <priteodoro01@gmail.com>
2023-09-28 19:10:15 -0400

    Adicionando arquivos curso Alura

---
+++ alura/react/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Logo from "./componentes/index"
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+       <Logo/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
