8c30f6b4eec75ec6ac513853c27a0ac91059a798
priscila <priteodoro01@gmail.com>
2023-10-04 19:06:54 -0400

    Estilizando pagina

--- alura/react/src/componentes/index.js
+++ alura/react/src/componentes/index.js
@@ -4,7 +4,11 @@ import './style.css'
 function Logo() {
      return(
         <div className='logo'>
-          <img src={logo} alt='Logotipo Alura Books'></img>
+          <img 
+          src={logo} 
+          alt='Logotipo Alura Books'
+          className='logo-img'
+          />
           <p><strong>Alura</strong>Books</p>
         </div>
      )
